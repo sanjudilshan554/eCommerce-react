9734702323e81018e111cacf8869e7c33c3b7b87
Sanju Dilshan <sanjudilshan554@gmail.com>
2025-02-23 16:34:50 +0530

    show sample categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
+const categories = [
+  "ALl",
+  "Graphic Cards",
+  "Laptop",
+  "Monitors",
+  "Power Supply",
+];
+
 function Home() {
   return (
     <div>
-      <div className="bg">
+      <div className="bg"></div>
 
-        
+      <div className="container mx-auto my-10 px-4">
+        <div className="flex gap-4">
+          {categories.map((cat) => {
+            return (
+              <button
+                className="bg-gray-300 py-2 px-4 rounded-md text-black active:scale-105 hover:bg-zinc-400 transition-all ease-in"
+                key={cat}
+              >
+                {cat}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
